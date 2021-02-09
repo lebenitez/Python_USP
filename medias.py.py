1fa67722cd0bd0c000c8d83e6e5b4e2636da72b8
Leandro Benitez <benitez.bpm@gmail.com>
2021-02-09 13:13:01 -0300

    Adicionando - Tarefa de programação: Lista de exercícios - 1

---
+++ medias.py.py
@@ -0,0 +1,24 @@
+"""
+Faça um programa em Python que receba quatro notas, calcule e imprima a média aritmética. Observe o exemplo abaixo:
+
+Exemplo:
+
+Entrada de Dados:
+Digite a primeira nota: 4
+
+Digite a segunda nota: 5
+
+Digite a terceira nota: 6
+
+Digite a quarta nota: 7
+
+Saída de Dados:
+A média aritmética é 5.5
+"""
+
+primeira_nota = float(input('Digite a primeira nota: '))
+segunda_nota = float(input('Digite a segunda nota: '))
+terceira_nota = float(input('Digite a terceira nota: '))
+quarta_nota = float(input('Digite a quarta nota: '))
+
+print('A média aritmética é:', (primeira_nota + segunda_nota + terceira_nota + quarta_nota) / 4)
---
+++ quadrado.py
@@ -0,0 +1,10 @@
+"""
+Faça um programa em Python que receba (entrada de dados) o valor
+correspondente ao lado de um quadrado, calcule e imprima (saída de dados) seu perímetro e sua área.
+
+Observação: a saída deve estar no formato: "perímetro: x - área: y"
+"""
+
+x = float(input('Digite o valor correspondente ao lado de um quadrado: '))
+
+print('Perímetro: ', x * 4, '- Área: ', x ** 2)
