9f4a22f1afd4dee3d2be668c76ee10523e6245ee
Leandro Benitez <benitez.bpm@gmail.com>
2021-02-09 13:25:02 -0300

    Correções realizadas para obter pontuação máxima

--- quadrado.py
+++ quadrado.py
@@ -7,4 +7,4 @@ Observação: a saída deve estar no formato: "perímetro: x - área: y"
 
 x = float(input('Digite o valor correspondente ao lado de um quadrado: '))
 
-print('Perímetro: ', x * 4, '- Área: ', x ** 2)
+print('perímetro:', x * 4,'- área:', x ** 2)
