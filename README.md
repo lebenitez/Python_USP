e38f66d5651ad0b33f8bf67dcabb189f5610119b
Leandro Benitez <benitez.bpm@gmail.com>
2021-02-09 10:49:33 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Python_USP
+ Curso Ciências da Computação USP - Python
---
+++ conversor_fahrenheit_celsius.py
@@ -0,0 +1,9 @@
+"""Conversos de Fahrenheit para Celsius"""
+
+temp_Fahrenheit = int(input('Digite a temperatura em Fahrenheit: '))
+
+# Armazena em variável o resultado da operação de conversão de Fahrenheit para Celsius
+# Na operação de divisão, utilizei o // para que o resultado seja um inteiro e não um float.
+temp_Celsius = (temp_Fahrenheit - 32) * 5 // 9
+
+print('A temperatura em Celsius é', temp_Celsius)
