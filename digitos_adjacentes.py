1128c30000a394b98077f9cc8d7c5ed3912ce72f
Leandro Benitez <benitez.bpm@gmail.com>
2021-02-14 22:10:05 -0300

    Update digitos_adjacentes.py

--- digitos_adjacentes.py
+++ digitos_adjacentes.py
@@ -4,14 +4,15 @@ num=int(input('Digite um número inteiro: '))
 # Gerando resto inicial da divisão
 rest1=num%10
 
-
-while num // 10 !=0: # Rode até que a variável num seja igual a zero
-      num = num // 10
-      rest = num % 10
-      if rest == rest1:
-            print('sim')
-            break
-      rest1 = rest
-      if num // 10 == 0:
-            print('não')
-
+if (num // 2) >= 5: # Condicional para quando digitarem números abaixo de 10
+      while num // 10 !=0: # Rode até que a variável num seja igual a zero
+            num = num // 10
+            rest = num % 10
+            if rest == rest1:
+                  print('sim')
+                  break
+            rest1 = rest
+            if num // 10 == 0:
+                  print('não')
+else:
+      print('não')
